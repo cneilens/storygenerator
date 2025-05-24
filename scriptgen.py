bc587ac7b79e29f11e9d3690485a7b844d82eb63
Colin Neilens <coneilen@microsoft.com>
2025-05-24 11:36:01 -0700

    uncomment out all the generator code

--- scriptgen.py
+++ scriptgen.py
@@ -30,56 +30,57 @@ def script_gen(story_file):
     print(f"Prompt: {prompt}")
     print(f"Base image prompt: {base_image_prompt}")
     
-    # print("Generating script...")
-    # response = client.responses.create(
-    #     model="gpt-4.1",
-    #     input=base_prompt + prompt,
-    #     temperature=0.7,
-    # )
+    print("Generating script...")
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=base_prompt + prompt,
+        temperature=0.7,
+    )
 
-    # print(response.output_text)
-    # jsonresponse = response.output_text.removeprefix("```json\n")
-    # jsonresponse = jsonresponse.removesuffix("\n```")
-    # jsonresponse = jsonresponse.replace("\'", "'")
-    # response = json.loads(jsonresponse)
-    # json.dump(response, open(response_file, "w"), indent=4)
+    print(response.output_text)
+    jsonresponse = response.output_text.removeprefix("```json\n")
+    jsonresponse = jsonresponse.removesuffix("\n```")
+    jsonresponse = jsonresponse.replace("\'", "'")
+    response = json.loads(jsonresponse)
+    json.dump(response, open(response_file, "w"), indent=4)
 
-    # for img_path in glob.glob("test-*.png"):
-    #     try:
-    #         os.remove(img_path)
-    #         print(f"Deleted {img_path}")
-    #     except OSError as e:
-    #         print(f"Error deleting {img_path}: {e}")
+    for img_path in glob.glob("test-*.png"):
+        try:
+            os.remove(img_path)
+            print(f"Deleted {img_path}")
+        except OSError as e:
+            print(f"Error deleting {img_path}: {e}")
 
-    # for index, prompt in enumerate(response.get("image_prompts")):
-    #     print (f"Generating image with prompt: {prompt}")
-    #     try:
-    #         result = client.images.edit(
-    #             model="gpt-image-1",
-    #             image=open(baseImage, "rb"),
-    #             prompt=base_image_prompt + prompt
-    #         )
-    #     except Exception as e:
-    #         print(f"Error generating image: {e}")
-    #         continue
-    #     image_base64 = result.data[0].b64_json
-    #     image_bytes = base64.b64decode(image_base64)
-    #     imageFileName = f"test-{index+1}.png"
-    #     print(f"Image {imageFileName} generated successfully.")
-    #     # Save the image to a file
-    #     with open(imageFileName, "wb") as f:
-    #         f.write(image_bytes)
+    for index, prompt in enumerate(response.get("image_prompts")):
+        print (f"Generating image with prompt: {prompt}")
+        try:
+            result = client.images.edit(
+                model="gpt-image-1",
+                image=open(baseImage, "rb"),
+                prompt=base_image_prompt + prompt
+            )
+        except Exception as e:
+            print(f"Error generating image: {e}")
+            continue
+        image_base64 = result.data[0].b64_json
+        image_bytes = base64.b64decode(image_base64)
+        imageFileName = f"test-{index+1}.png"
+        print(f"Image {imageFileName} generated successfully.")
+        # Save the image to a file
+        with open(imageFileName, "wb") as f:
+            f.write(image_bytes)
 
-    # with open(baseImage, "rb") as f1:
-    #     image_bytes = f1.read()
-    #     # Save the image to a file
-    #     with open("test-0.png", "wb") as f:
-    #         f.write(image_bytes)
+    with open(baseImage, "rb") as f1:
+        image_bytes = f1.read()
+        # Save the image to a file
+        with open("test-0.png", "wb") as f:
+            f.write(image_bytes)
+
+    # response = json.load(open(response_file, "r"))
 
     mp4_file, ext = os.path.splitext(story_file)
     mp4_file = mp4_file + ".mp4"
 
-    response = json.load(open(response_file, "r"))
     best_music = get_best_track_for_script(response.get("script"), hints=None, min_length=90, max_length=140, refreshCache=False)
     if best_music:
         music = best_music
